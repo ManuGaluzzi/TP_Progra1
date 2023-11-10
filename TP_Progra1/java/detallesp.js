45bde73fa68759e805bf18b4b570ca5e84c8a47a
antoniassilva <antoniasilva@udesa.edu.a>
2023-11-10 10:58:11 -0300

    pushantonia

--- TP_Progra1/java/detallesp.js
+++ TP_Progra1/java/detallesp.js
@@ -5,6 +5,17 @@ let ID = data.get("id_pelicula");
 
 
 let urldet = `https://api.themoviedb.org/3/movie/${ID}?api_key=${f534b4f9cb5b663c994b7b752531f5a9}`
+
 fetch (urldet)
 .then(function(response){
-    return response.json()})
+    return response.json()
+})
+.then(fuction(data)){
+    console.log(data);
+    let moviedet= document.querySelector(".marco");
+    let detalle=
+        <li>
+
+            <h2>${</h2>
+        </li>
+}
